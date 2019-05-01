7c7edcaf2994bcad6a3666d62b9866e9dc0ee1f4
Pavel Katskov <pasha_kackov@mail.ru>
2019-05-01 15:25:34 +0300

    add pip package

--- redforester/__init__.py
+++ redforester/__init__.py
@@ -6,7 +6,7 @@ import logging
 import json
 import hashlib
 from dataclasses import dataclass, field
-
+import typing
 
 class Session:
     """
@@ -48,6 +48,12 @@ class Request:
             async with session.request(self.method,
                                        f"{self.session.config.PROTOCOL}://{self.session.config.BASIC_URL}{self.url}",
                                        ) as response:
+                if response.status in [500, 404]:
+                    logging.critical(f'Request.async_send {await response.text()}')
+                    return response.status, await response.text()
+                elif response.status != 200:
+                    data = json.loads(await response.text())
+                    logging.error(f"Request.async_send, code: {data['code']}, message: {data['message']}")
                 return response.status, json.loads(await response.text())
 
     def send(self):
@@ -81,6 +87,12 @@ class Action(Request):
             async with session.request(self.method,
                                        f"{self.session.config.PROTOCOL}://{self.session.config.BASIC_URL}{self.url}",
                                        data=json.dumps(self.data), headers=headers) as response:
+                if response.status in [500, 404]:
+                    logging.critical(f'Action.async_send {await response.text()}')
+                    return response.status, await response.text()
+                elif response.status != 200:
+                    data = json.loads(await response.text())
+                    logging.error(f"Action.async_send, code: {data['code']}, message: {data['message']}")
                 return response.status, await response.text()
 
     def send(self):
@@ -144,7 +156,7 @@ class Users:
     def __init__(self, session: Session):
         self.session = session
 
-    async def __async_update(self, user: User):
+    async def async_update(self, user: User):
 
         if not user.current:
             logging.error('Вы не можете изменять данные другого пользователя!')
@@ -153,19 +165,19 @@ class Users:
             action = Action(self.session, 'PATCH', '/api/user', user.changes)
             response = await action.async_send()
             if response[0] != 200:
-                logging.error('Users.update' + str(response))
+                data = json.loads(response[1])
+                logging.error(f"Users.update, code: {data['code']}, message: {data['message']}")
 
-    def __sync_update(self, user: User):
-        self.session.loop.run_until_complete(self.__async_update(user))
+    def sync_update(self, user: User):
+        self.session.loop.run_until_complete(self.async_update(user))
 
-
-
-
-    async def __async_get(self):
+    async def async_get(self):
         request = Request(self.session, "GET", "/api/user")
         response = await request.async_send()
         if response[0] != 200:
-            logging.error('Users.get' + str(response))
+            data = json.loads(response[1])
+            logging.error(f"Users.get, code: {data['code']}, message: {data['message']}")
+
             return None
         else:
             args = {
@@ -184,21 +196,17 @@ class Users:
             user.changes.clear()
             return user
 
-    def __sync_get(self):
-        return self.session.loop.run_until_complete(self.__async_get())
-
-
-
-
+    def sync_get(self):
+        return self.session.loop.run_until_complete(self.async_get())
 
         # print('SET', key, value)
 
-
-    async def __async_get_by_id(self, user_id: str):
+    async def async_get_by_id(self, user_id: str):
         request = Request(self.session, "GET", f"/api/user/{user_id}")
         response = await request.async_send()
         if response[0] != 200:
-            logging.error('Users.get' + str(response))
+            data = json.loads(response[1])
+            logging.error(f"Users.get_by_id, code: {data['code']}, message: {data['message']}")
             return None
         else:
             args = {
@@ -217,26 +225,146 @@ class Users:
             user.changes.clear()
             return user
 
-    def __sync_get_by_id(self, user_id: str):
-        return self.session.loop.run_until_complete(self.get_by_id(user_id))
+    def sync_get_by_id(self, user_id: str):
+        return self.session.loop.run_until_complete(self.async_get_by_id(user_id))
+
+    # def __getattr__(self, item):
+    #     # super().__getattribute__(item)
+    #     if item == "get":
+    #         if self.session.sync:
+    #             return self.__sync_get
+    #         else:
+    #             return self.__async_get
+    #     elif item == "get_by_id":
+    #         if self.session.sync:
+    #             return self.__sync_get_by_id
+    #         else:
+    #             return self.__async_get_by_id
+    #     elif item == "update":
+    #         if self.session.sync:
+    #             return self.__sync_update
+    #         else:
+    #             return self.__async_update
 
 
-    def __getattr__(self, item):
-        # super().__getattribute__(item)
-        if item == "get":
-            if self.session.sync:
-                return self.__sync_get
+@dataclass
+class Map(DataNode):
+    id: str = ""
+    root_node_id: str = ""
+    owner: str = ""
+    owner_name: str = ""
+    owner_avatar: str = ""
+    layout: str = "LR"
+    public: bool = False
+    node_count: int = 0
+    user_count: int = 0
+    name: str = "noname"
+
+    def __init__(self, *args, **kwargs):
+        self.__dict__.update(kwargs)
+        print(self.__dict__)
+        # for kwarg in kwargs:
+        #     print(kwargs)
+        super().__init__()
+        self.changes.clear()
+
+
+
+
+class Maps:
+    """
+    Users - репозиторий для работы с объектами типа User.
+    Основная задача - реализация CRUD методов
+    """
+
+    def __init__(self, session: Session):
+        self.session = session
+
+    async def async_create(self, *maps: typing.List[Map]):
+        actions = []
+        print(maps)
+        for map in maps:
+            action = Action(self.session, 'POST', '/api/maps', map.changes)
+            actions.append(action)
+        sequence = Sequence(self.session, tuple(actions))
+
+        response = await sequence.async_send()
+        # print(response)
+        if response[0] != 200:
+            if response[0] != 404:
+                data = json.loads(response[1])
+                logging.error(f"Maps.update, code: {data['code']}, message: {data['message']}")
             else:
-                return self.__async_get
-        elif item == "get_by_id":
-            if self.session.sync:
-                return self.__sync_get_by_id
-            else:
-                return self.__async_get_by_id
-        elif item == "update":
-            if self.session.sync:
-                return self.__sync_update
-            else:
-                return self.__async_update
+                logging.error(f"Maps.update, " + str(response))
+
+    def sync_create(self, *maps: typing.List[Map]):
+        self.session.loop.run_until_complete(self.async_create(*maps))
 
 
+    async def async_update(self, *maps: typing.List[Map]):
+        actions = []
+        print(maps)
+        for map in maps:
+            if len(map.changes.keys()) > 0:
+                action = Action(self.session, 'PATCH', f'/api/maps/{map.id}', map.changes)
+                actions.append(action)
+
+        if len(actions) > 0:
+            sequence = Sequence(self.session, tuple(actions))
+            response = await sequence.async_send()
+            # print(response)
+            if response[0] != 200:
+                if response[0] != 404:
+                    data = json.loads(response[1])
+                    logging.error(f"Maps.update, code: {data['code']}, message: {data['message']}")
+                else:
+                    logging.error(f"Maps.update, " + str(response))
+
+    def sync_update(self, *maps: typing.List[Map]):
+        self.session.loop.run_until_complete(self.async_update(*maps))
+
+    async def async_get_by_id(self, map_id: str):
+        request = Request(self.session, "GET", f"/api/maps/{map_id}")
+        response = await request.async_send()
+        if response[0] != 200:
+            logging.error('Maps.get_by_id' + str(response))
+            return None
+        else:
+            # print(response)
+            args = {
+                "id": "" if not ("id" in response[1]) else response[1]["id"],
+                "root_node_id": "" if not ("root_node_id" in response[1]) else response[1]["root_node_id"],
+                "owner": "" if not ("owner" in response[1]) else response[1]["owner"],
+                "owner_name": "" if not ("owner_name" in response[1]) else response[1]["owner_name"],
+                "owner_avatar": "" if not ("owner_avatar" in response[1]) else response[1]["owner_avatar"],
+                "layout": "" if not ("layout" in response[1]) else response[1]["layout"],
+                "public": False if not ("public" in response[1]) else response[1]["public"],
+                "node_count": 0 if not ("node_count" in response[1]) else response[1]["node_count"],
+                "user_count": 0 if not ("user_count" in response[1]) else response[1]["user_count"],
+                "name": "" if not ("name" in response[1]) else response[1]["name"],
+
+            }
+            map = Map(**args)
+            map.changes.clear()
+            return map
+
+    def sync_get_by_id(self, map_id: str):
+        return self.session.loop.run_until_complete(self.async_get_by_id(map_id))
+
+    # def __getattr__(self, item):
+    #     # super().__getattribute__(item)
+    #     if item == "get":
+    #         if self.session.sync:
+    #             return self.__sync_get
+    #         else:
+    #             return self.__async_get
+    #     elif item == "get_by_id":
+    #         if self.session.sync:
+    #             return self.__sync_get_by_id
+    #         else:
+    #             return self.__async_get_by_id
+    #     elif item == "update":
+    #         if self.session.sync:
+    #             return self.__sync_update
+    #         else:
+    #             return self.__async_update
