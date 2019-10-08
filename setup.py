15c183a5ca4c8f62a38e3975d428914ac6b9bb08
Pavel Katskov <pasha_kackov@mail.ru>
2019-10-08 21:39:02 +0300

    add Node, Nodes classes

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="redforester",
-    version="0.1.6",
+    version="0.1.7",
     description="Async RedForester API module for Python 3.7+",
-    url="https://github.com/ichega/rfapi/",
+    url="https://github.com/ichega/redforester",
     author="Pavel Katskov",
     author_email="pasha_kackov@mail.ru",
     license="MIT",
